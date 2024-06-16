ba059be3e0645265368853116052042c9ae00228
ErroR404 <149897756+ErroR404-sources@users.noreply.github.com>
2024-06-17 00:11:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ $(tput setaf 6)   ☭ Народная армия непобедима!
 $(tput setaf 6)  
 $(tput setaf 6)   
 $(tput setaf 6)    
-$(tput setaf 6) COPYRIGHT 2024 ReyoServers Technology (https://reyo.run) & https://github.com/AvexXS & https://github.com/beastgamer81
+$(tput setaf 6) COPYRIGHT 2024 Quantumhost.xyz Technology (https://Quantuhost.xyz)
 
     ==========================================================================
     "  
